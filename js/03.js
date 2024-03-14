d7eca3324435a535daf567e0e5330bf2d5b8fba9
Thales <thales.marcelino@gmail.com>
2024-03-14 13:36:35 -0300

    feat: Creation of the js file and answer to question 03

---
+++ js/03.js
@@ -0,0 +1,29 @@
+//Questão A
+let a = 7;
+let proximoA = a + 2;
+console.log(proximoA); // Saída: 9
+
+//Questão B
+let b = 64;
+let proximoB = b * 2;
+console.log(proximoB); // Saída: 128
+
+//Questão C
+let c = 36;
+let proximoC = Math.pow(Math.sqrt(c) + 1, 2);
+console.log(proximoC); // Saída: 49
+
+//Questão D
+let d = 64;
+let proximoD = Math.pow(Math.sqrt(d) + 1, 2);
+console.log(proximoD); // Saída: 81
+
+//Questão E
+let e = 8;
+let proximoE = 8 + 5;
+console.log(proximoE); // Saída: 13
+
+//Questão F
+let f = 19;
+let proximoF = f + 1;
+console.log(proximoF); // Saída: 20
